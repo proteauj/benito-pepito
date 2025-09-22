246483ef914071b12d4b6dd8f1792c8bf22eb897
Julie Proteau <julie.proteau@hotmail.com>
2025-09-22 09:05:59 -0400

    fix error stripe

--- my-app/src/app/api/create-checkout-session/route.ts
+++ my-app/src/app/api/create-checkout-session/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2022-11-15',
+  apiVersion: '2025-08-27.basil',
 });
 
 export async function POST(request: Request) {

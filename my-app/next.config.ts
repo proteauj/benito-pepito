fa5ebd41bacf870e834ddc42497350764319437f
Julie Proteau <julie.proteau@hotmail.com>
2025-09-26 23:49:49 -0400

    Make import @ works

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -12,6 +12,17 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    // Ajouter des alias pour les chemins d'importation
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "./app"),
+      "@/components": path.resolve(__dirname, "./app/components"),
+      "@/data": path.resolve(__dirname, "./app/data"),
+      "@/i18n": path.resolve(__dirname, "./app/i18n"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;

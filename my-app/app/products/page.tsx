1383a15c96bb6d7a38f808742b4f8d3ac8ccbbb1
Julie Proteau <julie.proteau@hotmail.com>
2026-01-14 23:37:39 -0500

    change for paginatedProducts

--- my-app/app/products/page.tsx
+++ my-app/app/products/page.tsx
@@ -206,7 +206,7 @@ function ProductsContent() {
 
                     <button
                       onClick={() => setPage(p => p + 1)}
-                      disabled={endIndex >= filteredProducts.length}
+                      disabled={endIndex >= paginatedProducts.length}
                       className="px-4 py-2 bg-gray-200 rounded disabled:opacity-50"
                     >
                       {t('actions.next')}

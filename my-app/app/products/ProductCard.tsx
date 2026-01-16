47ce6e8b42bea757b109f9663f683720cb2a1d94
Julie Proteau <julie.proteau@hotmail.com>
2026-01-16 14:00:17 -0500

    Improve img weight

--- my-app/app/products/ProductCard.tsx
+++ my-app/app/products/ProductCard.tsx
@@ -3,20 +3,15 @@ import { Product } from '../../lib/db/types';
 export default function ProductCard({ product }: { product: Product }) {
   console.log(product.image, product.imageThumbnail);
   return (
-    <div className="bg-white rounded shadow overflow-hidden flex flex-col">
-      {/* IMAGE */}
+    <div className="bg-white shadow rounded flex flex-col">
       <img
         src={product.imageThumbnail || product.image}
         alt={product.title}
-        className="w-full h-auto object-contain"
-        loading="lazy"
+        className="w-full h-auto object-contain block"
       />
 
-      {/* TEXTE */}
       <div className="p-2">
-        <h2 className="text-base font-semibold leading-snug">
-          {product.title}
-        </h2>
+        <h2 className="text-sm font-semibold">{product.title}</h2>
         <p className="text-sm mt-1">{product.price} €</p>
       </div>
     </div>
